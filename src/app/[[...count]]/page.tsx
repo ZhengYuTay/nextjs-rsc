0000e2dd9935820ebc20d63461c4e826d66903aa
zyzy <tayzhengyu@gmail.com>
2024-02-27 17:19:22 +0800

    test

--- src/app/[[...count]]/page.tsx
+++ src/app/[[...count]]/page.tsx
@@ -1,22 +1,18 @@
-import { ServerRuntime } from "next";
 import Link from "next/link";
-import { useParams } from "next/navigation";
 import * as React from "react";
 
 interface ICountPageProps {}
 
-export const dynamicParams = false;
+export const dynamicParams = true;
 export const dynamic = "force-static";
 
 const CountPage: React.FunctionComponent<ICountPageProps> = (props: any) => {
   return (
     <div>
       {props.params?.count}
-      <button className="bg-red-400">
-        <Link href={`/${(Number(props.params?.count?.toString()) || 0) + 1}`}>
-          Click for next page
-        </Link>
-      </button>
+      <Link href={`/${(Number(props.params?.count?.toString()) || 0) + 1}`}>
+        <button className="bg-red-400">Click for next page</button>
+      </Link>
     </div>
   );
 };
