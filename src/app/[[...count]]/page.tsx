573398939897918a46e1ec641c95338696d0b7d3
zyzy <tayzhengyu@gmail.com>
2024-02-27 17:25:47 +0800

    test

--- src/app/[[...count]]/page.tsx
+++ src/app/[[...count]]/page.tsx
@@ -3,7 +3,7 @@ import * as React from "react";
 
 interface ICountPageProps {}
 
-export const dynamicParams = true;
+export const dynamicParams = false;
 export const dynamic = "error";
 
 const CountPage: React.FunctionComponent<ICountPageProps> = (props: any) => {
