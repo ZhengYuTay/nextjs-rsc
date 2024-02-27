05ce193cbb8f740a87828fda67eb58eb8008f81f
zyzy <tayzhengyu@gmail.com>
2024-02-27 15:01:54 +0800

    sample

---
+++ src/app/[count]/page.tsx
@@ -0,0 +1,23 @@
+"use client";
+import Link from "next/link";
+import { useParams } from "next/navigation";
+import * as React from "react";
+
+interface ICountPageProps {}
+
+const CountPage: React.FunctionComponent<ICountPageProps> = (props) => {
+  const params = useParams();
+  return (
+    <div>
+      {params?.count}
+
+      <button className="bg-red-400">
+        <Link href={`/${(Number(params?.count.toString()) || 0) + 1}`}>
+          Click for next page
+        </Link>
+      </button>
+    </div>
+  );
+};
+
+export default CountPage;
